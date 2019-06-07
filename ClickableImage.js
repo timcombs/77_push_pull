a6888243082e12081bd67c1a84dc6136072bfb87
Tim Combs <tim00combs@gmail.com>
2019-06-06 19:00:25 -0700

    working on creating a clickable Image component using hooks

--- ClickableImage.js
+++ ClickableImage.js
@@ -1,14 +1,20 @@
 // going to make this a clickable image using Hooks
 
-/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
- */
+import React, { useState } from 'react';
+
+export default function ClickableImage(imgPath) {
+  const [imgURL, setImgURL] = useState(imgPath);
+
+  return (
+    <>
+    <img src={logo} className="App-logo" alt="logo" />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >img</a>
+     </>
+
+  );
+}
